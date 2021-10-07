84bf06371a3587d046aaaeda487ff1d1b12862ec
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2021-10-07 12:21:25 +0700

    Create Ver0.cs

---
+++ Year2021_1/01076011 OPERATING SYSTEMS/Case Study #1/Ver0.cs
@@ -0,0 +1,96 @@
+//1 Thread
+//ถ้าไม่ใส่ Join() จะเกิด "เวลา - ผลลัพธ์เท่า 0"
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+namespace Problem01
+{
+    class Program
+    {
+        static byte[] Data_Global = new byte[1000000000];
+        static long Sum_Global = 0;
+
+        static int ReadData()
+        {
+            int returnData = 0;
+            FileStream fs = new FileStream("Problem01.dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            try
+            {
+                Data_Global = (byte[])bf.Deserialize(fs);
+            }
+            catch (SerializationException se)
+            {
+                Console.WriteLine("Read Failed:" + se.Message);
+                returnData = 1;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            return returnData;
+        }
+        static void sum()
+        {
+            int G_index = 0;
+            for (int i = 0; i < 1000000000; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            Thread th1 = new Thread(sum);
+            /* Read data from file */
+            Console.Write("Data read...");
+            int y = ReadData();
+            if (y == 0)
+            {
+                Console.WriteLine("Complete.");
+            }
+            else
+            {
+                Console.WriteLine("Read Failed!");
+            }
+
+            /* Start */
+            Console.Write("\n\nWorking...");
+            sw.Start();
+            th1.Start();
+            th1.Join();
+            sw.Stop();
+            
+            Console.WriteLine("Done.");
+
+            /* Result */
+            Console.WriteLine("Summation result: {0}", Sum_Global);
+            Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
