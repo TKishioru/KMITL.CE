fa2aa842920c7ebabb6811210373580850ea43ec
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2021-10-07 10:49:31 +0700

    Ver 3.

---
+++ Year2021_1/01076011 OPERATING SYSTEMS/Case Study #1/Ver3.cs
@@ -0,0 +1,235 @@
+//MultiThread 6 Core
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+namespace Problem01
+{
+    class Program
+    {
+        static byte[] Data_Global = new byte[1000000000];
+        static long Sum_Global1 = 0, Sum_Global2 = 0, Sum_Global3 = 0, Sum_Global4 = 0, Sum_Global5 = 0, Sum_Global6 = 0;
+
+        static int ReadData()
+        {
+            int returnData = 0;
+            FileStream fs = new FileStream("Problem01.dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            try
+            {
+                Data_Global = (byte[])bf.Deserialize(fs);
+            }
+            catch (SerializationException se)
+            {
+                Console.WriteLine("Read Failed:" + se.Message);
+                returnData = 1;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            return returnData;
+        }
+        static void sum1()
+        {
+            int G_index = 0;
+            for (int i = 0; i < 166666667; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global1 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global1 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global1 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global1 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+
+        static void sum2()
+        {
+            int G_index = 1;
+            for (int i = 0; i < 166666666; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global2 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global2 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global2 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global2 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+        static void sum3()
+        {
+            int G_index = 2;
+            for (int i = 0; i < 166666666; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global3 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global3 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global3 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global3 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+        static void sum4()
+        {
+            int G_index = 3;
+            for (int i = 0; i < 166666666; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global4 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global4 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global4 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global4 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+        static void sum5()
+        {
+            int G_index = 4;
+            for (int i = 0; i < 166666666; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global5 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global5 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global5 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global5 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+        static void sum6()
+        {
+            int G_index = 5;
+            for (int i = 0; i < 166666666; i++)
+            {
+                if (Data_Global[G_index] % 2 == 0)
+                {
+                    Sum_Global6 -= Data_Global[G_index];
+                }
+                else if (Data_Global[G_index] % 3 == 0)
+                {
+                    Sum_Global6 += (Data_Global[G_index] * 2);
+                }
+                else if (Data_Global[G_index] % 5 == 0)
+                {
+                    Sum_Global6 += (Data_Global[G_index] / 2);
+                }
+                else if (Data_Global[G_index] % 7 == 0)
+                {
+                    Sum_Global6 += (Data_Global[G_index] / 3);
+                }
+                Data_Global[G_index] = 0;
+                G_index += 6;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            Thread th1 = new Thread(sum1);
+            Thread th2 = new Thread(sum2);
+            Thread th3 = new Thread(sum3);
+            Thread th4 = new Thread(sum4);
+            Thread th5 = new Thread(sum5);
+            Thread th6 = new Thread(sum6);
+            /* Read data from file */
+            Console.Write("Data read...");
+            int y = ReadData();
+            if (y == 0)
+            {
+                Console.WriteLine("Complete.");
+            }
+            else
+            {
+                Console.WriteLine("Read Failed!");
+            }
+
+            /* Start */
+            Console.Write("\n\nWorking...");
+            sw.Start();
+            th1.Start();
+            th2.Start();
+            th3.Start();
+            th4.Start();
+            th5.Start();
+            th6.Start();
+            th1.Join();
+            th2.Join();
+            th3.Join();
+            th4.Join();
+            th5.Join();
+            th6.Join();
+            sw.Stop();
+            Console.WriteLine("Done.");
+
+            /* Result */
+            //For 6 Core
+            Console.WriteLine("Summation result: {0}", Sum_Global1 + Sum_Global2 + Sum_Global3 + Sum_Global4 + Sum_Global5 + Sum_Global6);
+            Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+    }
+}
