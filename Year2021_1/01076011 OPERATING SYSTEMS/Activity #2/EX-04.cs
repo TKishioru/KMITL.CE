460bfd6698d94a83ebcc0168c7dbbb4213f78f40
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2021-10-28 15:18:31 +0700

    Complete

--- Year2021_1/01076011 OPERATING SYSTEMS/Activity #2/EX-04.cs
+++ Year2021_1/01076011 OPERATING SYSTEMS/Activity #2/EX-04.cs
@@ -4,37 +4,64 @@ using System.Threading;
 
 namespace OS_Sync_04
 {
-	class Program
-	{
-		private static string x = "";
-		private static int exitflag = 0;
+    class Program
+    {
+        private static string x = "";
+        private static int exitflag = 0;
+        private static int print = 1;
+        private static int key = 0;
+        private static Semaphore s;
+        static void ThReadX()
+        {
 
-		static void ThReadX()
-		{
-			while (exitflag == 0)
-				Console.WriteLine("X = {0}", x);
-		}
+            while (exitflag == 0)
+            {
+                s.WaitOne();
+                if (print == 0) Console.WriteLine("X = {0}", x);
+                print = 1;
+                key = 0;
+                s.Release();
+            }
+        }
 
-		static void ThWriteX()
-		{
-			string xx;
-			while (exitflag == 0)
-			{
-				Console.WriteLine("Input: ");
-				xx = Console.ReadLine();
-				if (xx == "exit")
-					exitflag = 1;
-				else
-					x = xx;
-			}
-		}
-			static void Main(string[] args)
-			{
-				Thread A = new Thread(ThReadX);
-				Thread B = new Thread(ThWriteX);
+        static void ThWriteX()
+        {
+            //Console.WriteLine("ThWriteX");
 
-				A.Start();
-				B.Start();
-			}
-		}
-	}
+            string xx;
+            while (exitflag == 0)
+            {
+                if (key == 0)
+                {
+                    Console.Write("Input: ");
+                    xx = Console.ReadLine();
+                    if (xx == "exit")
+                    {
+                        s.WaitOne();
+                        exitflag = 1;
+                        Console.WriteLine("Thread 1 exit");
+                        s.Release();
+                    }
+                    else if (xx != "")
+                    {
+                        s.WaitOne();
+                        x = xx;
+                        key = 1;
+                        print = 0;
+                        s.Release();
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread A = new Thread(ThReadX);
+            Thread B = new Thread(ThWriteX);
+            s = new Semaphore(1, 1);
+
+            A.Start();
+            B.Start();
+        }
+    }
+}
