8a6135b1c3ebfe17e92960c7b01d80e1a4b8c5ec
KHAMPEERADA P <khampeerada44@gmail.com>
2021-11-06 16:00:57 +0700

    กำลัง DeQ

--- Year2021_1/01076011 OPERATING SYSTEMS/Case Study #2/TK01.cs
+++ Year2021_1/01076011 OPERATING SYSTEMS/Case Study #2/TK01.cs
@@ -11,7 +11,8 @@ namespace OS_Problem_02
         static int Front = 0;
         static int Back = 0;
         static int Count = 0;
-        private static int updateFlag = 0;
+        private static int updateFlagEn = 0;
+        private static int updateFlagDe = 0;
         private static Object _LockEn = new object();
         private static Object _LockDe = new object();
 
@@ -30,81 +31,74 @@ namespace OS_Problem_02
 
         static int DeQueue()
         {
-            int x = 0;
-            x = TSBuffer[Front];
-            Front++;
-            Front %= 10;
-            Count -= 1;
-
+            lock (_LockDe) {
+                int x = 0;
+                x = TSBuffer[Front];
+                Front++;
+                Front %= 10;
+                Count -= 1;
+            }
             return x;
         }
 
         //EnQueue 01 011 : 1 11
         static void th01()
         {
-            //lock (_LockEn)
-            if (updateFlag == 0)
+            if (updateFlagEn == 0)
             {
                 for (int i = 1; i < 51; i++)
                 {
                     EnQueue(i);
                     Thread.Sleep(5);
                 }
-                updateFlag = 1;
+                updateFlagEn = 1;
             }
         }
         static void th011()
         {
-            //lock (_LockEn)
-            if (updateFlag == 0)
+            if (updateFlagEn == 0)
             {
                 for (int i = 100; i < 151; i++)
                 {
                     EnQueue(i);
                     Thread.Sleep(5);
                 }
-                updateFlag = 1;
+                updateFlagEn = 1;
             }
         }
 
-        //ใช้ DeQueue 02 : 2 21 22
+        //DeQueue 02 : 2 21 22
         static void th02(object t)
         {
-            int j;/*
-           lock (_LockDe)
-                if (updateFlag == 1 && Count != 0)
-                {*/
-            for (int i = 0; i < 60; i++)
+            int j;
+
+            if (updateFlagDe == 1 && Count != 0)
             {
-                if (Count > 0)
+                for (int i = 0; i < 60; i++)
                 {
+                    while (Count <= 0)
+                    {
+                        Console.WriteLine("Wait");
+                    }
                     j = DeQueue();
                     Console.Write("--- {0}", Count);
                     Console.WriteLine("j={0}, thread:{1}", j, t);
                     Thread.Sleep(100);
                 }
-                else
-                {
-                    Console.WriteLine("--- Loss {0}", i);
-                    //Console.WriteLine("Wait");
-                    //i--;
-                }
-
+                updateFlagDe = 0;
             }
-            /*    updateFlag = 0;
-            }*/
         }
         static void Main(string[] args)
         {
             Thread t1 = new Thread(th01);
             Thread t11 = new Thread(th011);
-            //Thread t2 = new Thread(th02);
+            Thread t2 = new Thread(th02);
             //Thread t21 = new Thread(th02);
             //Thread t22 = new Thread(th02);
 
             t1.Start();
             t11.Start();
-            //t2.Start(1);
+            t2.Start(1);
             //t21.Start(2);
             //t22.Start(3);
         }
