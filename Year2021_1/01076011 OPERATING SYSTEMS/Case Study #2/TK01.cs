5d8ad5e77069e76f7020bb1a01c95fd8c825f393
KHAMPEERADA P <khampeerada44@gmail.com>
2021-11-06 15:30:32 +0700

    complete enQ

--- Year2021_1/01076011 OPERATING SYSTEMS/Case Study #2/TK01.cs
+++ Year2021_1/01076011 OPERATING SYSTEMS/Case Study #2/TK01.cs
@@ -12,66 +12,70 @@ namespace OS_Problem_02
         static int Back = 0;
         static int Count = 0;
         private static int updateFlag = 0;
-        private static Object _Lock = new object();
-        
+        private static Object _LockEn = new object();
+        private static Object _LockDe = new object();
+
         //Function
         static void EnQueue(int eq)
         {
-            TSBuffer[Back] = eq;
-            Back++;
-            Back %= 10;
-            Count += 1;
+            lock (_LockEn)
+            {
+                TSBuffer[Back] = eq;
+                Back++;
+                Back %= 10;
+                Count += 1;
+                Console.WriteLine("At count {0} [Data real : {1}]", Count, eq);
+            }
         }
 
         static int DeQueue()
         {
             int x = 0;
-                x = TSBuffer[Front];
-                Front++;
-                Front %= 10;
-                Count -= 1;
-         
+            x = TSBuffer[Front];
+            Front++;
+            Front %= 10;
+            Count -= 1;
+
             return x;
         }
 
         //EnQueue 01 011 : 1 11
         static void th01()
         {
-            lock (_Lock)
-                if (updateFlag == 0)
+            //lock (_LockEn)
+            if (updateFlag == 0)
+            {
+                for (int i = 1; i < 51; i++)
                 {
-                    for (int i = 1; i < 51; i++)
-                    {
-                        EnQueue(i);
-                        Thread.Sleep(5);
-                    }
-                    updateFlag = 1;
+                    EnQueue(i);
+                    Thread.Sleep(5);
                 }
+                updateFlag = 1;
+            }
         }
-
         static void th011()
         {
-            lock (_Lock)
-                if (updateFlag == 0)
+            //lock (_LockEn)
+            if (updateFlag == 0)
+            {
+                for (int i = 100; i < 151; i++)
                 {
-                    for (int i = 100; i < 151; i++)
-                    {
-                        EnQueue(i);
-                        Thread.Sleep(5);
-                    }
-                    updateFlag = 1;
+                    EnQueue(i);
+                    Thread.Sleep(5);
                 }
+                updateFlag = 1;
+            }
         }
 
         //ใช้ DeQueue 02 : 2 21 22
         static void th02(object t)
-        { 
+        {
             int j;/*
-           lock (_Lock)
+           lock (_LockDe)
                 if (updateFlag == 1 && Count != 0)
                 {*/
-                    for (int i = 0; i < 60; i++)
-                    {
+            for (int i = 0; i < 60; i++)
+            {
                 if (Count > 0)
                 {
                     j = DeQueue();
@@ -85,22 +89,22 @@ namespace OS_Problem_02
                     //Console.WriteLine("Wait");
                     //i--;
                 }
-                
+
             }
-                /*    updateFlag = 0;
-                }*/
+            /*    updateFlag = 0;
+            }*/
         }
         static void Main(string[] args)
         {
             Thread t1 = new Thread(th01);
-            //Thread t11 = new Thread(th011);
-            Thread t2 = new Thread(th02);
+            Thread t11 = new Thread(th011);
+            //Thread t2 = new Thread(th02);
             //Thread t21 = new Thread(th02);
             //Thread t22 = new Thread(th02);
 
             t1.Start();
-            //t11.Start();
-            t2.Start(1);
+            t11.Start();
+            //t2.Start(1);
             //t21.Start(2);
             //t22.Start(3);
         }
