cb73e34e226d5bc442b0add49656cf66b098cf05
KHAMPEERADA P <khampeerada44@gmail.com>
2022-02-17 22:32:37 +0700

    hw

---
+++ Year2021_2/01076025 SOFTWARE STUDIO/04 Javascript/01.js
@@ -0,0 +1,8 @@
+function func_date() {
+    let color = ["red", "yellow", "pink", "green", "orange", "blue", "purple"];
+    const d = new Date();
+    document.getElementById("fdate").innerHTML = d;
+
+    document.getElementById('fdate').style.g
+
+}
