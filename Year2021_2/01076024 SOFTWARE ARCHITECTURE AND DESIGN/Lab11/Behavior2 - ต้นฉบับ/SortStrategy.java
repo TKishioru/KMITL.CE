688f6df751ef32a6b46517113503d3d879e65b7a
KHAMPEERADA P <54303135+TKishioru@users.noreply.github.com>
2022-04-22 00:01:43 +0700

    -

---
+++ Year2021_2/01076024 SOFTWARE ARCHITECTURE AND DESIGN/Lab11/Behavior2 - ต้นฉบับ/SortStrategy.java
@@ -0,0 +1,8 @@
+package edu.parinya.softarchdesign.behavior2;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
---
+++ Year2021_2/01076024 SOFTWARE ARCHITECTURE AND DESIGN/Lab11/Behavior2 - แก้/NameSortStrategy.java
@@ -0,0 +1,9 @@
+package edu.parinya.softarchdesign.behavior2;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class NameSortStrategy implements SortStrategy {
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+
+}
